a952f1a37a3d97689c3848e6f0863ed1296c8330
Marcus Ontiveros <marcus.ontiveros@gmail.com>
2014-06-21 13:25:11 -0700

    Added EF, poolio dbcontext, and added some entities

---
+++ Poolio/Poolio.Data/Entities/Matchups.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poolio.Data.Entities
+{
+    public class Matchups
+    {
+        public int MatchupId { get; set; }
+
+        public int HomeTeamId { get; set; }
+
+        public int AwayTeamId { get; set; }
+
+        public int WinningTeamId { get; set; }
+
+        public int LosingTeamId { get; set; }
+
+        public ICollection<Week> Weeks { get; set; }
+
+        public ICollection<Team> Teams { get; set; }
+
+    }
+}
--- Poolio/Poolio.Data/Interfaces/IPoolioContext.cs
+++ Poolio/Poolio.Data/Interfaces/IPoolioContext.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Poolio.Domain
+namespace Poolio.Data.Interfaces
 {
-    public class Class1
+    public interface IPoolioContext
     {
+     
     }
 }
